b656d0ccdf466ea16ca69c8cb709e9cd97c86dcc
Paige Edrington <paige.edrington@gmail.com>
2014-08-27 12:56:43 -0700

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <ul id="menu">
-<li><a href="Index.php">home</a></li>
-<li><a href="About.php">about</a></li>
-<li><a href="Party.php">wedding party</a></li>
-<li><a href="RSVP.php">rsvp</a></li>
+<li><a href="index.php">home</a></li>
+<li><a href="about.php">about</a></li>
+<li><a href="party.php">wedding party</a></li>
+<li><a href="rsvp.php">rsvp</a></li>
 <li><a href="http://www.target.com">registry</a></li>
 </ul>
