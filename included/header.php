2d008051adaa8da8ce7cd65c562a3cbc0e7681b5
Paige Edrington <paige@synapsestudios.com>
2014-09-16 16:57:46 -0700

    New header page

---
+++ included/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?php echo $title; ?></title> 
+  <link href="/stylesheets/wedding.css" rel="stylesheet">
+</head>
+
+<body>
+
+<?php include "menu.php"; ?>
+
+<div id="main">
