76e02805ca6c5cb0642e5218cd2a0fe0c642ec8f
Paige Edrington <paige@synapsestudios.com>
2014-09-16 17:39:10 -0700

    Add favicon

--- included/header.php
+++ included/header.php
@@ -3,6 +3,7 @@
 <head>
   <title><?php echo $title; ?></title> 
   <link href="/stylesheets/wedding.css" rel="stylesheet">
+  <link rel="icon" href="../favicon.ico" type="image/x-icon"/>
 </head>
 
 <body>
