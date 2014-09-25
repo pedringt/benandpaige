c3009e384852dbec18ee97d4c1334169ec205f3d
Paige Edrington <paige@synapsestudios.com>
2014-09-25 13:49:16 -0700

    Some content and css
    Add some content and change css up a bit.

--- pages/about.php
+++ pages/about.php
@@ -3,7 +3,16 @@ $title="about";
 include "../included/header.php";
  ?>
 
-		<h1>about the wedding</h1> 
+		<h1>8.15.15</h1> 
+		<br >
+		<h2>location & information</h2>
+		<p>A small ceremony and reception for immediate family and close friends will take place in Bloomington, IN on August 15th, 2015. 
+		The ceremony will be held at the Bloomington courthouse, followed by dinner and drinks at Nick's 
+		Bar & Grill on Kirkwood Avenue.</p>
+		<p>Another ceremony will follow in New Zealand at the shooting location of The Shire from
+		The Lord of the Rings. Anyone is welcome to attend this as well.</p>
+		
+		
 
 
 <?php include "../included/footer.php"; ?>
