b9270de08f62b3238e591667eadf1e50fe31f6e2
Paige Edrington <paige@synapsestudios.com>
2014-10-06 11:41:40 -0700

    Update about content

--- pages/about.php
+++ pages/about.php
@@ -9,8 +9,7 @@ include "../included/header.php";
 		<p>A small ceremony and reception for immediate family and close friends will take place in Bloomington, IN on August 15th, 2015. 
 		The ceremony will be held at the Bloomington courthouse, followed by dinner and drinks at Nick's 
 		Bar & Grill on Kirkwood Avenue.</p>
-		<p>Another ceremony will follow in New Zealand at the shooting location of The Shire from
-		The Lord of the Rings. Anyone is welcome to attend this as well.</p>
+		<p>Another ceremony will follow in New Zealand.</p>
 		
 		
 
