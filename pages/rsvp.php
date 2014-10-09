4c36a3952667923427e6bd60582b2de269037891
Paige Edrington <paige@synapsestudios.com>
2014-10-09 16:07:51 -0700

    RSVP style stuff

--- pages/rsvp.php
+++ pages/rsvp.php
@@ -4,13 +4,14 @@ include "../included/header.php";
  ?>
 
 <h1>rsvp</h1> 
-<p>rsvp here</p>
 
+<div class="container">
 <form action="/included/insert.php" method="post">
 Name: <input type="text" name="name" required><br>
 Number of guests attending: <input type="number" name="number" required><br>
 Dietary restrictions: <input type="text" name="food"><br>
 <input type="submit">
 </form>
+</div>
 
 <?php include "../included/footer.php"; ?>
